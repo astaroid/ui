c6bd00297d945c55445541af5f786495d833e3a0
daniel_onyenwee <danielonyenwee@gmail.com>
2022-08-14 20:08:58 +0100

    ➕ add component "auth-tab-controller"

---
+++ src/lib/component/auth-tab-controller.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte"
+
+    export let tab:"sign up"|"sign in" = "sign in"
+    export let theme:"light"|"dark" = "light"
+    export let width:number = 100
+    export let unit:"px"|"mm"|"pt"|"cm"|"pc"|"in"|"%" = "%"
+
+    const dispatcher = createEventDispatcher()
+
+    const setTab = (tabToSet:"sign up"|"sign in") => {
+        tab = tabToSet
+        dispatcher("onTabSelected", { tab })
+    }
+</script>
+
+<nav style="width: {`${width}${unit}`}" data-theme={theme}>
+    <button on:click={() => setTab("sign in")} data-current-tab={ tab == "sign in" ? "true": "false" } >Sign in</button>
+    <button on:click={() => setTab("sign up")} data-current-tab={ tab == "sign up" ? "true": "false" } >Sign up</button>
+</nav>
+
+<style lang="less">
+    nav {
+        width: 100%;
+        height: 60px;
+        border-top-left-radius: 7px;
+        border-top-right-radius: 7px;
+        background-color: #fafafa;
+        border-style: solid;
+        border-width: 1px;
+        border-color: #d6d6d6;
+        box-shadow: 0px 1.25px 1px 0px #f5f5f5;
+        display: flex;
+        &[data-theme="dark"] {
+            background-color: #242526;
+            border-style: solid;
+            border-color: #474849;
+            border-width: 1px;
+            box-shadow: 0 0px 0 #282828;
+            button {
+                color: white;
+                &[data-current-tab="true"] {
+                    border-color: white;
+                }
+            }
+        }
+        button {
+            height: 100%;
+            width: 50%;
+            font-size: 18px;
+            background-color: transparent;
+            border-color: transparent;
+            border-style: solid;
+            border-width: 0px;
+            border-bottom-width: 4px;
+            font-weight: bold;
+            color: #303030;
+            &[data-current-tab="true"] {
+                border-color: #303030;
+            }
+            &:focus {
+                outline: none;
+            }
+        }
+    }
+</style>
--- src/lib/component/index.ts
+++ src/lib/component/index.ts
@@ -1,3 +1,5 @@
+import AuthTabController from "./auth-tab-controller.svelte"
+
 export default {
-    
+    AuthTabController
 }

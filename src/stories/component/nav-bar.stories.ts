541116a6fb8ab31f14f3d9e251f5881ebd04b2f0
daniel_onyenwee <danielonyenwee@gmail.com>
2022-08-23 20:47:23 +0100

    ➕ add story "nav-bar"

---
+++ src/stories/component/nav-bar.stories.ts
@@ -0,0 +1,62 @@
+import NavBar from "../../lib/component/nav-bar.svelte"
+
+export default {
+    title: "Component/Navbar",
+    component: NavBar,
+    parameters: {
+        layout: 'fullscreen',
+        backgrounds: {
+            default: "light",
+            values: [
+                { name: 'light', value: '#fafafa' },
+                { name: 'dark', value: '#303030' },
+            ]
+        }
+    },
+    argTypes: {
+        onTabSelected: { 
+            action: 'onTabSelected', 
+            description: "The event emitter when a tab is selected"
+        },
+        theme: {
+            name: "theme",
+            type: { required: true, name: "string" },
+            description: "The theme of the component",
+            defaultValue: "light",
+            control: {
+                type: "radio",
+                options: [ "light", "dark" ]
+            }
+        },
+        tab: {
+            name: "tab",
+            type: { required: true, name: "string" },
+            description: "The selected tab",
+            defaultValue: "home",
+            control: {
+                type: "select",
+                options: [ "home", "search", "assets", "activity","profile" ]
+            }
+        }
+    }
+}
+
+const Template = (args:any) => ({
+    Component: NavBar,
+    props: args,
+    on: {
+       onTabSelected: args.onTabSelected
+    }
+})
+
+export const Container = Template.bind({}) 
+
+export const DarkContainer = Template.bind({}) 
+DarkContainer.args = {
+    theme: "dark"
+}
+DarkContainer.parameters = {
+    backgrounds: {
+        default: 'dark'
+    }
+}

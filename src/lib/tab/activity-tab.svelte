5ddde970bab5c78db120a0715cff32fa640c0fd7
daniel_onyenwee <danielonyenwee@gmail.com>
2022-10-11 00:26:23 +0100

    ➕ add component "tooltip" and ✔ set section[data-theme="dark"]>nav background-color to rgb(25, 25, 25)

--- src/lib/tab/activity-tab.svelte
+++ src/lib/tab/activity-tab.svelte
@@ -4,6 +4,7 @@
     import { createEventDispatcher } from "svelte"
     import ActivityMessage from "../component/activity-message.svelte"
     import Menu from "../component/menu.svelte"
+    import Tooltip from "../component/tooltip.svelte"
 
     type ActivtyMessageType = "SOLD_ASSET_MESSAGE"|
         "BOUGHT_CRYSTAL_MESSAGE"|
@@ -224,11 +225,13 @@
         position="absolute"/>
     <nav>
         <span>Activity</span>
-        <button use:clickoutside={{ enabled: true, callback: () => isMenuButtonClicked = false }} on:click={toggleMenu}>
-            <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M3.625 7.5C3.625 8.12132 3.12132 8.625 2.5 8.625C1.87868 8.625 1.375 8.12132 1.375 7.5C1.375 6.87868 1.87868 6.375 2.5 6.375C3.12132 6.375 3.625 6.87868 3.625 7.5ZM8.625 7.5C8.625 8.12132 8.12132 8.625 7.5 8.625C6.87868 8.625 6.375 8.12132 6.375 7.5C6.375 6.87868 6.87868 6.375 7.5 6.375C8.12132 6.375 8.625 6.87868 8.625 7.5ZM12.5 8.625C13.1213 8.625 13.625 8.12132 13.625 7.5C13.625 6.87868 13.1213 6.375 12.5 6.375C11.8787 6.375 11.375 6.87868 11.375 7.5C11.375 8.12132 11.8787 8.625 12.5 8.625Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
-            </svg>
-        </button>
+        <Tooltip theme={theme} label="Settings">
+            <button use:clickoutside={{ enabled: true, callback: () => isMenuButtonClicked = false }} on:click={toggleMenu}>
+                <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3.625 7.5C3.625 8.12132 3.12132 8.625 2.5 8.625C1.87868 8.625 1.375 8.12132 1.375 7.5C1.375 6.87868 1.87868 6.375 2.5 6.375C3.12132 6.375 3.625 6.87868 3.625 7.5ZM8.625 7.5C8.625 8.12132 8.12132 8.625 7.5 8.625C6.87868 8.625 6.375 8.12132 6.375 7.5C6.375 6.87868 6.87868 6.375 7.5 6.375C8.12132 6.375 8.625 6.87868 8.625 7.5ZM12.5 8.625C13.1213 8.625 13.625 8.12132 13.625 7.5C13.625 6.87868 13.1213 6.375 12.5 6.375C11.8787 6.375 11.375 6.87868 11.375 7.5C11.375 8.12132 11.8787 8.625 12.5 8.625Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
+                </svg>
+            </button>
+        </Tooltip>
     </nav>
     <main>
         {#if !loading }
@@ -370,7 +373,7 @@
             border-color: #4e4c4c;
             box-shadow: none;
             nav {
-                background-color: rgb(16, 20, 25);
+                background-color: rgb(25, 25, 25);
                 border-color: #4e4c4c;
                 color: rgb(253, 253, 253);
                 button {

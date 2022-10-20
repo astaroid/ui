732b8e01881d819bbf7d1a8f343e754f4c37f539
daniel_onyenwee <danielonyenwee@gmail.com>
2022-10-20 01:53:25 +0100

    ➕ add story "asset-skeleton-container"

---
+++ src/stories/component/asset-skeleton-container.stories.ts
@@ -0,0 +1,44 @@
+import AssetSkeletonContainer from "../../lib/component/asset-skeleton-container.svelte"
+
+export default {
+    title: "Component/Asset Skeleton Container",
+    component: AssetSkeletonContainer,
+    parameters: {
+        backgrounds: {
+            default: "light",
+            values: [
+                { name: 'light', value: '#fafafa' },
+                { name: 'dark', value: '#303030' },
+            ]
+        }
+    },
+    argTypes: {
+        theme: {
+            name: "theme",
+            type: { required: true, name: "string" },
+            description: "The theme of the component",
+            defaultValue: "light",
+            control: {
+                type: "radio",
+                options: [ "light", "dark" ]
+            }
+        }
+    }
+}
+
+const Template = (args:any) => ({
+    Component: AssetSkeletonContainer,
+    props: args
+})
+
+export const Container = Template.bind({}) 
+
+export const DarkContainer = Template.bind({}) 
+DarkContainer.args = {
+    theme: "dark"
+}
+DarkContainer.parameters = {
+    backgrounds: {
+        default: 'dark'
+    }
+}

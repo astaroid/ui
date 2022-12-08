12fbeff8f671e2d1664e85348d1efd42f12d357c
daniel_onyenwee <danielonyenwee@gmail.com>
2022-12-08 22:03:09 +0100

    ➕ add story "user-card"

---
+++ src/stories/component/user-card.stories.ts
@@ -0,0 +1,74 @@
+import UserCard from "../../lib/component/user-card.svelte"
+
+export default {
+    title: "Component/Account Tab component/User Card",
+    component: UserCard,
+    parameters: {
+        backgrounds: {
+            default: "light",
+            values: [
+                { name: 'light', value: '#fafafa' },
+                { name: 'dark', value: '#303030' },
+            ]
+        }
+    },
+    argTypes: {
+        theme: {
+            name: "theme",
+            type: { required: true, name: "string" },
+            description: "The theme of the component",
+            defaultValue: "light",
+            control: {
+                type: "select",
+                options: [ "light", "dark", "system" ]
+            }
+        },
+        email: {
+            type: { name: "string", required: true },
+            description: "The user email",
+            defaultValue: "usernameemail@gmail.com",
+            control: { type: "text" }
+        },
+        username: {
+            type: { name: "string", required: true },
+            description: "The user username",
+            defaultValue: "user_001",
+            control: { type: "text" }
+        },
+        profileImage: {
+            type: { name: "string" },
+            description: "The user profile image",
+            defaultValue: "/default_profile_blank.jpg",
+            control: { type: "text" }
+        },
+        coinsNumber: {
+            type: { name: "number" },
+            description: "The user total coins",
+            defaultValue: 0,
+            control: { type: "number" }
+        },
+        assetsNumber: {
+            type: { name: "number" },
+            description: "The user total assets",
+            defaultValue: 0,
+            control: { type: "number" }
+        },
+    }
+}
+
+const Template = (args:any) => ({
+    Component: UserCard,
+    props: args
+})
+
+export const Container = Template.bind({})
+
+export const DarkContainer = Template.bind({})
+DarkContainer.args = {
+    theme: "dark"
+}
+DarkContainer.parameters = {
+    backgrounds: {
+        default: "dark"
+    }
+}

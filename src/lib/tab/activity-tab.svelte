ebc91a6c911302d2636fe3a077e7420538a28e50
daniel_onyenwee <danielonyenwee@gmail.com>
2022-12-09 15:06:58 +0100

    🔄 refactor component support for menu system theme

--- src/lib/tab/activity-tab.svelte
+++ src/lib/tab/activity-tab.svelte
@@ -46,11 +46,11 @@
             type: "item",
             id: "bought-crystal-message-btn",
             text: `${showBoughtCrystalMessages ? "Hide" : "Show"} all Bought Crystal Messages`,
-            "icon data": showBoughtCrystalMessages ? `<svg xmlns="http://www.w3.org/2000/svg" style="width: 18.5px; height: 18.5px; fill: ${theme == "light" ? "#303030" : "#c1c2bd"}" viewBox="0 0 16 16">
+            "icon data": showBoughtCrystalMessages ? `<svg xmlns="http://www.w3.org/2000/svg" style="width: 18.5px; height: 18.5px; fill: ${menuItemIconTheme == "light" ? "#303030" : "#c1c2bd"}" viewBox="0 0 16 16">
                 <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"/>
                 <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z"/>
                 <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"/>
-            </svg>` : `<svg style="width: 18.5px; height: 18.5px; fill: ${theme == "light" ? "#303030" : "#c1c2bd"}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+            </svg>` : `<svg style="width: 18.5px; height: 18.5px; fill: ${menuItemIconTheme == "light" ? "#303030" : "#c1c2bd"}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                 <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
                 <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
             </svg>` 
@@ -59,11 +59,11 @@
             type: "item",
             id: "sold-asset-messages-btn",
             text: `${showSoldAssetMessages ? "Hide" : "Show"} all Sold Asset Messages`,
-            "icon data": showSoldAssetMessages ? `<svg xmlns="http://www.w3.org/2000/svg" style="width: 18.5px; height: 18.5px; fill: ${theme == "light" ? "#303030" : "#c1c2bd"}" viewBox="0 0 16 16">
+            "icon data": showSoldAssetMessages ? `<svg xmlns="http://www.w3.org/2000/svg" style="width: 18.5px; height: 18.5px; fill: ${menuItemIconTheme == "light" ? "#303030" : "#c1c2bd"}" viewBox="0 0 16 16">
                 <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"/>
                 <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z"/>
                 <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"/>
-            </svg>` : `<svg style="width: 18.5px; height: 18.5px; fill: ${theme == "light" ? "#303030" : "#c1c2bd"}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+            </svg>` : `<svg style="width: 18.5px; height: 18.5px; fill: ${menuItemIconTheme == "light" ? "#303030" : "#c1c2bd"}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                 <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
                 <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
             </svg>` 
@@ -72,11 +72,11 @@
             type: "item",
             id: "transaction-fee-messages-btn",
             text: `${showTransactionFeeMessages ? "Hide" : "Show"} all Transaction Fee Messages`,
-            "icon data": showTransactionFeeMessages ? `<svg xmlns="http://www.w3.org/2000/svg" style="width: 18.5px; height: 18.5px; fill: ${theme == "light" ? "#303030" : "#c1c2bd"}" viewBox="0 0 16 16">
+            "icon data": showTransactionFeeMessages ? `<svg xmlns="http://www.w3.org/2000/svg" style="width: 18.5px; height: 18.5px; fill: ${menuItemIconTheme == "light" ? "#303030" : "#c1c2bd"}" viewBox="0 0 16 16">
                 <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"/>
                 <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z"/>
                 <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"/>
-            </svg>` : `<svg style="width: 18.5px; height: 18.5px; fill: ${theme == "light" ? "#303030" : "#c1c2bd"}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+            </svg>` : `<svg style="width: 18.5px; height: 18.5px; fill: ${menuItemIconTheme == "light" ? "#303030" : "#c1c2bd"}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                 <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
                 <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
             </svg>` 
@@ -85,11 +85,11 @@
             type: "item",
             id: "payout-messages-btn",
             text: `${showPayoutMessages ? "Hide" : "Show"} all Payout Messages`,
-            "icon data": showPayoutMessages ? `<svg xmlns="http://www.w3.org/2000/svg" style="width: 18.5px; height: 18.5px; fill: ${theme == "light" ? "#303030" : "#c1c2bd"}" viewBox="0 0 16 16">
+            "icon data": showPayoutMessages ? `<svg xmlns="http://www.w3.org/2000/svg" style="width: 18.5px; height: 18.5px; fill: ${menuItemIconTheme == "light" ? "#303030" : "#c1c2bd"}" viewBox="0 0 16 16">
                 <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"/>
                 <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z"/>
                 <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"/>
-            </svg>` : `<svg style="width: 18.5px; height: 18.5px; fill: ${theme == "light" ? "#303030" : "#c1c2bd"}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+            </svg>` : `<svg style="width: 18.5px; height: 18.5px; fill: ${menuItemIconTheme == "light" ? "#303030" : "#c1c2bd"}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                 <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
                 <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
             </svg>` 
@@ -102,11 +102,7 @@
             id: "close-btn",
             type: "item",
             text: "Close Activity Tab",
-            color: "#f03e3e",
-            "icon data": `<svg xmlns="http://www.w3.org/2000/svg" style="fill: #f03e3e; width: 13.25px; height: 13.25px" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"/>
-                <path fill-rule="evenodd" d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z"/>
-            </svg>`
+            color: "#f03e3e"
         }
     ])
 
@@ -189,12 +185,13 @@
 
     messages = filterMessages(unFilteredMessages)
 
-    let hasOpend = false
+    let hasOpened = false
     let sectionWidth = 350
     let menuXPosition = sectionWidth - 192
     let menuYPosition = 43
     let showMenu = false
     let isMenuButtonClicked = false
+    let menuItemIconTheme:string = theme
     let menuItems:Array<MenuItemType> = menuItemsList()
     let reactiveTrigger = false
     let sectionElement:HTMLElement = null
@@ -203,23 +200,33 @@
         menuXPosition = sectionWidth - 192   
         if (!Array.isArray(messages))
             messages = Array<ActivtyMessage>()
-        if (show && !hasOpend) {
+        if (show && !hasOpened) {
             dispatcher("onOpened")
-            hasOpend = true
+            hasOpened = true
         }
         messages = filterMessages(unFilteredMessages)
         menuItems = menuItemsList()
     }
 
     onMount(() => {
+        if (theme == "system") {
+            menuItemIconTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"
+            menuItems = menuItemsList()
+        }
         sectionElement.classList.remove("slide-out-anim")
         sectionElement.addEventListener("animationend", (e) => {
             if (e.animationName.includes("slide-out")) {
                 sectionElement.classList.remove("slide-out-anim")
                 show = false
                 dispatcher("onClosed")
-                hasOpend = false
+                hasOpened = false
             } 
+        })
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", (e) => {
+            if (theme == "system") {
+                menuItemIconTheme = e.matches ? "dark" : "light"
+                menuItems = menuItemsList()
+            }
         })
     })
 </script>

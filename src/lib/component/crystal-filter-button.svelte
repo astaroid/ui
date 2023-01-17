2fb33daa6f923a98b5d2cf2c889de5a794c9832f
daniel_onyenwee <danielonyenwee@gmail.com>
2023-01-17 03:04:44 +0100

    🔄 refactor component code to position the menu

--- src/lib/component/crystal-filter-button.svelte
+++ src/lib/component/crystal-filter-button.svelte
@@ -19,6 +19,7 @@
     const dispatcher = createEventDispatcher()
 
     const menuWidth = 200
+    
     const menuHeight = 150
     
     const onOrderByChange = () => {
@@ -89,7 +90,6 @@
     let showMenu = false
     let isFilterByButtonClicked = false
     let filterByButtonHight = 0
-    let filterByButtonWidth = 0
     let reactiveTrigger = false
     let menuYPosition = 0
     let menuXPosition = 0
@@ -98,9 +98,9 @@
     let menuItems:Array<MenuItemType> = menuItemsList()
 
     $: if (reactiveTrigger || !reactiveTrigger) {
-        if (filterByButton && window) {
-            menuXPosition = filterByButton.getBoundingClientRect().x - filterByButtonWidth -  (menuWidth/2 + 35 /* Padding Right */)
-            menuYPosition = filterByButton.getBoundingClientRect().y - filterByButtonHight/2 - menuHeight/2 
+        if (filterByButton) {
+            menuXPosition = filterByButton.getBoundingClientRect().x - menuWidth
+            menuYPosition = filterByButton.getBoundingClientRect().y + filterByButtonHight/1.65 - menuHeight
         }
     }
 
@@ -109,10 +109,8 @@
             menuItemIconTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"
             menuItems = menuItemsList()
         }
-        if (filterByButton) {
-            menuXPosition = filterByButton.getBoundingClientRect().x -  (menuWidth/2 + 20 /* Padding Right */)
-            menuYPosition = filterByButton.getBoundingClientRect().y + filterByButtonHight + 10 /* Padding Top  */
-        }
+        menuXPosition = filterByButton.getBoundingClientRect().x - menuWidth
+        menuYPosition = filterByButton.getBoundingClientRect().y + filterByButtonHight/1.65 - menuHeight
         window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", (e) => {
             if (theme == "system") {
                 menuItemIconTheme = e.matches ? "dark" : "light"
@@ -132,10 +130,10 @@
                 <path d="M3.5 2.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 11.293V2.5zm3.5 1a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z"/>
             </svg>
         </button>
-        <button bind:clientWidth={filterByButtonWidth} bind:clientHeight={filterByButtonHight} bind:this={filterByButton} data-button="filterby-button" use:clickoutside={{ enabled: true, callback: () => isFilterByButtonClicked = false }} on:click={toggleMenu}>
+        <button bind:clientHeight={filterByButtonHight} bind:this={filterByButton} data-button="filterby-button" use:clickoutside={{ enabled: true, callback: () => isFilterByButtonClicked = false }} on:click={toggleMenu}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                 <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z"/>
-              </svg>
+            </svg>
         </button>
     </main>
 </section>
@@ -147,8 +145,8 @@
     placement="start"
     menuItems={menuItems}
     bind:show={showMenu}
-    on:onMenuItemClicked={menuItemClicked}
-    position="absolute"/>
+    on:onMenuItemClicked={menuItemClicked}  
+    position="fixed"/>
 <style lang="less">
     section[data-container="wrapper"] {
         position: sticky;

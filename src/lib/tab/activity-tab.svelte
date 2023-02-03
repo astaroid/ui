d8600864cbed82c438d50f54680ce7f4f6cf0f4d
daniel_onyenwee <danielonyenwee@gmail.com>
2023-02-03 15:00:49 +0100

    ➕ add close button to tab

--- src/lib/tab/activity-tab.svelte
+++ src/lib/tab/activity-tab.svelte
@@ -242,6 +242,13 @@
         on:onMenuItemClicked={menuItemClicked}
         position="absolute"/>
     <nav>
+        <Tooltip theme={theme} label="Close">
+            <button on:click={close}>
+                <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                </svg>
+            </button>
+        </Tooltip>
         <span>Activity</span>
         <Tooltip theme={theme} label="Settings">
             <button use:clickoutside={{ enabled: true, callback: () => isMenuButtonClicked = false }} on:click={toggleMenu}>

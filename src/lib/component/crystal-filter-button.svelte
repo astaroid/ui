be008a2d3b543687662ab5ffc51cce744f64aedf
daniel_onyenwee <danielonyenwee@gmail.com>
2023-02-22 07:13:10 +0100

    ❌ remove Menu component

--- src/lib/component/crystal-filter-button.svelte
+++ src/lib/component/crystal-filter-button.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-    import { createEventDispatcher, onMount } from "svelte"
-    import { clickoutside } from '@svelteuidev/composables'
-    import Menu from "../component/menu.svelte"
+    import { createEventDispatcher } from "svelte"
 
     interface MenuItemType {
         id: string
@@ -17,10 +15,6 @@
     export let filterBy:"price"|"volume"|"unique" = "volume"
 
     const dispatcher = createEventDispatcher()
-
-    const menuWidth = 200
-    
-    const menuHeight = 150
     
     const onOrderByChange = () => {
         if (orderBy == "ascending")
@@ -29,96 +23,6 @@
             orderBy = "ascending"
         dispatcher("onOrderByChange", { orderBy })
     }
-
-    const toggleMenu = () => {
-        showMenu = !showMenu
-        isFilterByButtonClicked = true
-    }
-
-    const menuItemsList = ():Array<MenuItemType> => ([
-        {
-            type: "label",
-            id: "label",
-            text: "Filter By"
-        },
-        {
-            type: "item",
-            id: "filter-volume-btn",
-            text: `Volume`,
-            "icon data": filterBy == "volume" ? `<svg xmlns="http://www.w3.org/2000/svg" style="width: 18.5px; height: 18.5px; fill: ${menuItemIconTheme == "light" ? "#303030" : "#c1c2bd"}" viewBox="0 0 16 16">
-                <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
-            </svg>` : null
-        },
-        {
-            type: "item",
-            id: "filter-price-btn",
-            text: `Price`,
-            "icon data": filterBy == "price" ? `<svg xmlns="http://www.w3.org/2000/svg" style="width: 18.5px; height: 18.5px; fill: ${menuItemIconTheme == "light" ? "#303030" : "#c1c2bd"}" viewBox="0 0 16 16">
-                <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
-            </svg>` : null
-        },
-        {
-            type: "item",
-            id: "filter-unique-btn",
-            text: `Unique`,
-            "icon data": filterBy == "unique" ? `<svg xmlns="http://www.w3.org/2000/svg" style="width: 18.5px; height: 18.5px; fill: ${menuItemIconTheme == "light" ? "#303030" : "#c1c2bd"}" viewBox="0 0 16 16">
-                <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
-            </svg>` : null
-        }
-    ])
-
-    const clickedOutsideMenu = () => {
-        if (!isFilterByButtonClicked)
-            showMenu = false
-        else 
-            showMenu = true
-    }
-
-    const menuItemClicked = (e:CustomEvent<{ id:string }>) => {
-        let id = e.detail.id
-        reactiveTrigger = !reactiveTrigger
-        if (id == "filter-price-btn") 
-            filterBy = "price"
-        else if (id == "filter-volume-btn")
-            filterBy = "volume"
-        else if (id == "filter-unique-btn")
-            filterBy = "unique"
-        menuItems = menuItemsList()
-        dispatcher("onFilterByChange", { filterBy } )
-    }
-
-    let showMenu = false
-    let isFilterByButtonClicked = false
-    let filterByButtonHight = 0
-    let reactiveTrigger = false
-    let menuYPosition = 0
-    let menuXPosition = 0
-    let filterByButton:HTMLButtonElement = null
-    let menuItemIconTheme:string = theme
-    let menuItems:Array<MenuItemType> = menuItemsList()
-
-    $: if (reactiveTrigger || !reactiveTrigger) {
-        if (filterByButton) {
-            menuXPosition = filterByButton.getBoundingClientRect().x - menuWidth
-            menuYPosition = filterByButton.getBoundingClientRect().y + filterByButtonHight/1.65 - menuHeight
-        }
-    }
-
-    onMount(() => {
-        if (theme == "system") {
-            menuItemIconTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"
-            menuItems = menuItemsList()
-        }
-        menuXPosition = filterByButton.getBoundingClientRect().x - menuWidth
-        menuYPosition = filterByButton.getBoundingClientRect().y + filterByButtonHight/1.65 - menuHeight
-        window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", (e) => {
-            if (theme == "system") {
-                menuItemIconTheme = e.matches ? "dark" : "light"
-                menuItems = menuItemsList()
-            }
-        })
-        window.addEventListener("resize", () => reactiveTrigger = !reactiveTrigger)
-    })
 </script>
 <section data-container="wrapper">
     <main data-theme={theme}>
@@ -130,23 +34,13 @@
                 <path d="M3.5 2.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 11.293V2.5zm3.5 1a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z"/>
             </svg>
         </button>
-        <button bind:clientHeight={filterByButtonHight} bind:this={filterByButton} data-button="filterby-button" use:clickoutside={{ enabled: true, callback: () => isFilterByButtonClicked = false }} on:click={toggleMenu}>
+        <button data-button="filterby-button">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                 <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z"/>
             </svg>
         </button>
     </main>
 </section>
-<Menu 
-    theme={theme}
-    x={menuXPosition}
-    y={menuYPosition}
-    on:onClickedOutside={clickedOutsideMenu}
-    placement="start"
-    menuItems={menuItems}
-    bind:show={showMenu}
-    on:onMenuItemClicked={menuItemClicked}  
-    position="fixed"/>
 <style lang="less">
     section[data-container="wrapper"] {
         position: sticky;

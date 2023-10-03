a1eda547872e35fc1f215561f7110e765639ec9e
daniel-onyenwee <danielonyenwee@gmail.com>
2023-10-03 13:10:40 +0100

    ❌ delete all the svelte files

--- src/lib/tab/index.ts
+++
@@ -1,9 +0,0 @@
-import AuthTab from "./auth-tab.svelte"
-import AccountTab from "./account-tab.svelte"
-import ActivityTab from "./activity-tab.svelte"
-
-export default {
-    AuthTab,
-    ActivityTab,
-    AccountTab
-}
